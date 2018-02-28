360b1bac8c7e7d56d4a0bffaf2db0c507dd27597
Stephanie Storey <storey662@icloud.com>
2018-02-28 09:54:50 -0600

    Initial Commit

---
+++ istApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  istApp
+//
+//  Created by Stephanie Storey on 2/1/18.
+//  Copyright © 2018 Stephanie Storey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBOutlet weak var openMenu: UIBarButtonItem!
+        leadingConstraint.constant = 0
+}
+
